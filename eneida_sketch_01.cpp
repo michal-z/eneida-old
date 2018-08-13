52873b3766feea922674f7218ac9af674296c0e3
Michal Ziulek <michalziulek@gmail.com>
2018-08-13 20:46:42 +0200

    Add files via upload

---
+++ eneida_sketch_01.cpp
@@ -0,0 +1,26 @@
+namespace Sketch01
+{
+
+struct demo
+{
+    int x;
+};
+
+static void
+Initialize(demo& Demo, directx12& Dx)
+{
+}
+
+static void
+Shutdown(demo& Demo, directx12& Dx)
+{
+}
+
+static void
+Update(demo& Demo, directx12& Dx, double FrameTime, float FrameDeltaTime)
+{
+    ImGui::ShowDemoWindow();
+}
+
+} // namespace FractalFlames
+// vim: set ts=4 sw=4 expandtab:
